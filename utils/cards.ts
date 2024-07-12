145f3b9f28331999c6f9add3f84775521e189835
Matias P. <matias.penailillob@gmail.com>
2024-07-12 13:29:55 -0400

    chore: add note comment

--- utils/cards.ts
+++ utils/cards.ts
@@ -3,6 +3,6 @@ export const normalizeCardName = (cardName: string): string => {
         .toLowerCase()                  // Convierte a minúsculas
         .normalize('NFD')               // Descompone caracteres con tilde
         .replace(/[\u0300-\u036f]/g, '')// Elimina marcas de diacríticos (acentos)
-        .replace(/\s+/g, '-');          // Reemplaza espacios por guiones
+        .replace(/\s+/g, '-');          // Reemplaza espacios por guiones (nota: igual existe replaceAll)
 }
 export const setSlugCards = (cards: string[]) => cards.map(card => normalizeCardName(card));
