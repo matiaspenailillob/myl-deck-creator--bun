01673b1f5fb83eba07e5d312144c23eca0903100
Matias P. <matias.penailillob@gmail.com>
2024-07-12 13:28:02 -0400

    refactor: change slug card functions for normalizeCardName

--- utils/cards.ts
+++ utils/cards.ts
@@ -1,2 +1,8 @@
-export const slugCardNameFormat = (cardName: string) => cardName.toLowerCase().replace(' ', '-')
-export const setSlugCards = (cards: string[]) => cards.map(card => slugCardNameFormat(card));
+export const normalizeCardName = (cardName: string): string => {
+    return cardName
+        .toLowerCase()                  // Convierte a minúsculas
+        .normalize('NFD')               // Descompone caracteres con tilde
+        .replace(/[\u0300-\u036f]/g, '')// Elimina marcas de diacríticos (acentos)
+        .replace(/\s+/g, '-');          // Reemplaza espacios por guiones
+}
+export const setSlugCards = (cards: string[]) => cards.map(card => normalizeCardName(card));
