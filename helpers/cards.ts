87b219b174256cd8dfc2f84f80b7b8093d50e0e3
Matias P. <matias.penailillob@gmail.com>
2024-07-22 22:45:01 -0400

    refactor(cards): add optional cardRules and implementation

--- helpers/cards.ts
+++ helpers/cards.ts
@@ -4,9 +4,9 @@ import type {Card} from "../models/cards.ts";
 import {CARD_TYPES} from "../enums/card-types.ts";
 import {multiSelectOptionsBuilder} from "../utils/options.ts";
 
-export const removeBannedCards = (cards: Card[], { bannedCards }: CardRules) => {
+export const removeBannedCards = (cards: Card[], {bannedCards}: CardRules) => {
     const bannedCardsSlugged = setSlugCards(bannedCards);
-    return cards.filter(({ slug }) => !bannedCardsSlugged.includes(slug))
+    return cards.filter(({slug}) => !bannedCardsSlugged.includes(slug))
 }
 
 export const buildCardMultiSelectOptions = (cards: Card[]) => {
@@ -19,21 +19,31 @@ export const buildCardMultiSelectOptions = (cards: Card[]) => {
         }
     })
 
-    return multiSelectOptionsBuilder({ message, options })
+    return multiSelectOptionsBuilder({message, options})
 }
 
 export const getCardsByType = (cards: Card[], type: CARD_TYPES) => cards.filter(card => card.type === type)
 
-export const isUniqueCard = ({ ability }: Card) => ability?.toLowerCase().includes('carta única');
+export const isUniqueCard = ({ability}: Card) => ability?.toLowerCase().includes('carta única');
 
 // TODO: WIP
-export const multiplyCards = (cards: Card[], { oneCopyCards, twoCopyCards }: CardRules): Card[] => {
+export const multiplyCards = (cards: Card[], cardRules?: CardRules): Card[] => {
+    // Inicializar el arreglo resultante
+    const result: Card[] = [];
+
+    if(!cardRules) {
+        cards.forEach(card => {
+            result.push(card, card, card);
+        });
+
+        return result;
+    }
+
+    const { oneCopyCards = [], twoCopyCards = [] } = cardRules;
+
     // Convertir las listas de cartas a sus slugs
     const oneCopyCardSlugged = setSlugCards(oneCopyCards);
     const twoCopyCardSlugged = setSlugCards(twoCopyCards);
-
-    // Inicializar el arreglo resultante
-    const result: Card[] = [];
 
     // Procesar cada carta
     cards.forEach(card => {
